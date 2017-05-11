3e0efbe0def6215fc527ab901c7292a1a751faf4
Andreas Krieger <Andreas.Krieger@tuwien.ac.at>
2017-05-11 15:42:23 +0200

    #4124 Update to Moodle 3.3.0_rc2

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017051000;
-$plugin->requires  = 2016120500;
+$plugin->version  = 2017051100;
+$plugin->requires  = 2017050900;
 $plugin->component = 'gradereport_gradedist';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = "v3.3.0";
