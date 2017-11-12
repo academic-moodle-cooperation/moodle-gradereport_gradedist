a2c918b9e2dec34634c984e49a5e23d74f02fea3
Andreas Krieger <Andreas.Krieger@tuwien.ac.at>
2017-11-12 16:10:07 +0100

    TravisCI: push verion.php to signal latest changes

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017111200;
+$plugin->version  = 2017111300;
 $plugin->requires  = 2017111300;
 $plugin->component = 'gradereport_gradedist';
 $plugin->maturity  = MATURITY_STABLE;
