6b9289dcae2a276c89e9dd085540303fa62c084b
Andreas Krieger <Andreas.Krieger@tuwien.ac.at>
2018-05-11 13:46:35 +0200

    #5113 Update version.php to bump for cluster update

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2018051100;
+$plugin->version  = 2018051101;
 $plugin->requires  = 2017111300;
 $plugin->component = 'gradereport_gradedist';
 $plugin->maturity  = MATURITY_ALPHA;
