e7c724078ca227b668f548d495764be0b4114bec
Andreas Krieger <Andreas.Krieger@tuwien.ac.at>
2018-12-06 12:36:54 +0100

    #5617 Increment required postgres version for travis-ci automated build

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2018120600;
+$plugin->version  = 2018120601;
 $plugin->requires  = 2018112800;
 $plugin->component = 'gradereport_gradedist';
 $plugin->maturity  = MATURITY_STABLE;
