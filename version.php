7ff90c79754f98df78a11a739e4ae7499d5099a9
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2022-07-28 17:16:21 +0200

    postgres 12, version.php, changelog

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2022032100;
+$plugin->version  = 2022032101;
 $plugin->requires  = 2022030800;
 $plugin->component = 'gradereport_gradedist';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "v4.0";
+$plugin->release   = "v4.1";
