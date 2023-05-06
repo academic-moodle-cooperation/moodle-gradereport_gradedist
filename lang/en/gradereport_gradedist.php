6e016ddfbc8f3710c07c717ed5ea3c5c3e6f745a
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2023-05-06 09:38:22 +0200

    sort lang file

--- lang/en/gradereport_gradedist.php
+++ lang/en/gradereport_gradedist.php
@@ -23,48 +23,36 @@
  * @copyright     2014 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license       http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
+defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Grade distribution';
 $string['pluginname_help'] = 'This report shows the distribution of grades over a particular grade item depending on the lower bounds defined for the grade letters. The preview functionality allows you to see immediately how changes to the lower boundaries of grade letters affect the grade distribution. If you are satisfied with the new lower boundaries the definitions of the grade letters can be permanently changed.
 Note: The definition of grade letters applies to all grade items in the course.';
-
 $string['gradedist:view'] = 'View grade distribution';
 $string['gradedist:edit'] = 'Manage grade distribution';
-
 $string['gradeitem'] = 'Grading column';
 $string['coursesum'] = 'Sum for the course';
-
 $string['labelgroup'] = 'Focus view on group';
-
 $string['labelgrouping'] = 'Focus view on grouping';
 $string['nogroupingentry'] = 'No grouping';
-
 $string['gradeletter'] = 'Grade letters';
-
 $string['gradeboundary'] = 'Lower boundary in %';
 $string['gradeboundary_help'] = 'The setting determines the minimum percentage over which grades will be assigned to the grade letter.';
 $string['gradeboundary_new'] = 'New lower boundary in %';
 $string['gradeboundary_new_help'] = 'You can see the effects of new lower boundaries on the distribution of grades. Lower boundaries have to be numbers with max. 2 decimal points.';
-
 $string['chart'] = 'Chart';
-
 $string['printchart'] = 'Print chart';
 $string['downloadpng'] = 'PNG image';
 $string['downloadjpeg'] = 'JPEG image';
 $string['downloadpdf'] = 'PDF document';
 $string['downloadsvg'] = 'Download SVG vector image';
 $string['contextbuttontitle'] = 'Chart context menu';
-
 $string['exportasimage'] = 'Download chart as';
-
 $string['description'] = 'Labelling';
 $string['absolut'] = 'Absolute';
 $string['percent'] = 'Percent';
-
 $string['columns'] = 'Bars';
 $string['actualcolumns'] = 'current grade letters';
 $string['newcolumns'] = 'new grade letters';
-
 $string['interval'] = 'The lower boundary has to be in between 0 and 100.';
 $string['decimals'] = 'The lower boundary has to be a number from 0 to 100. Maximum two decimal places are allowed.';
 $string['predecessor'] = 'The lower boundary of a grade letter has to be smaller than the boundary of his predecessor.';
@@ -72,26 +60,20 @@ $string['coverage'] = 'The new grade distribution does not cover all grades!';
 $string['coverage_export'] = 'Items not included by letters:';
 $string['actcoverage'] = 'Items not included by current letters: ';
 $string['newcoverage'] = 'Items not included by new letters: ';
-
 $string['changeletters'] = 'Save changed grade letters';
 $string['confirm'] = 'Save changed grade letters';
-
 $string['notification'] = 'Note: The definition of grade letters applies to all grade items in the course.';
-
 $string['boundaryerror'] = 'Some entries for new grade letters may be invalid. There must be input on each field';
 $string['saved'] = 'Grade letters successfully changed.';
-
 $string['export'] = 'Download data as';
 $string['download'] = 'Download';
 $string['xlsx'] = 'xlsx';
 $string['ods'] = 'ods';
 $string['csv'] = 'csv';
-
 $string['showgradeitem'] = 'Display grade item';
 $string['showgradeitem_description'] = 'Show grade display type as choice for grade item.';
 $string['showgradeitemtype'] = 'Display grade item type';
 $string['showgradeitemtype_help'] = 'Show grade item type like quiz, assignment etc.';
-
 $string['category'] = 'Gradecategory';
 $string['a'] = ' (absolute)';
 $string['p'] = ' (%)';
@@ -99,20 +81,13 @@ $string['sum'] = 'Sum';
 $string['actualgrade'] = 'Grade (current)';
 $string['newgrade'] = 'Grade (new)';
 $string['points'] = 'Points ({$a})';
-
 $string['highchartsmissing'] = 'To view the diagram download version 4.x of the Highcharts library from <a href="http://code.highcharts.com/">http://code.highcharts.com/</a>. Copy both together, the core module highcharts.src.js and the exporting.src.js module, or alternatively if you prefer their minified versions highcharts.js and exporting.js respectively, into the directory moodleroot/grade/report/gradedist/js.';
-
-// Events.
 $string['gradedistviewed'] = 'Grade distribution viewed';
 $string['gradedistviewed_description'] = 'The user with id {$a->userid} viewed the grade distribution.';
-
 $string['gradedistdownloaded'] = 'Current grade distribution downloaded';
 $string['gradedistdownloaded_description'] = 'The user with id {$a->userid} downloaded the current grade distribution.';
-
 $string['confirmationtableviewed'] = 'Confirmation table viewed';
 $string['confirmationtableviewed_description'] = 'The user with id {$a->userid} viewed the grade distribution confirmation table.';
-
 $string['newletterssubmitted'] = 'New grade letters submitted';
 $string['newletterssubmitted_description'] = 'The user with id {$a->userid} submitted the new grade letters.';
-
 $string['privacy:null_reason'] = 'This plugin does not store any personal information. It presents an interface to visually display and export the grade distribution of the graded elements within a course.';
