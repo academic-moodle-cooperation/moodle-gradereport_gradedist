1ad7065efdeb6de2489444d291d3db395c90d214
marxc01 <marx.clemens@univie.ac.at>
2024-02-21 11:31:14 +0100

    Fix code checker issues; add namespace, coverage

--- tests/lib_test.php
+++ tests/lib_test.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace gradereport_gradedist;
+
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
@@ -27,16 +29,18 @@ require_once($CFG->dirroot . '/grade/report/gradedist/lib.php');
  * @author     Clemens Marx
  * @copyright  2024 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @covers     \gradereport_gradedist\lib
  */
 class lib_test extends \advanced_testcase {
     /**
      * report to test
-     * @var grade_report_gradedist $report
+     * @var \grade_report_gradedist $report
      */
     protected $report;
     /**
      * course to test
-     * @var stdClass $course
+     * @var \stdClass $course
      */
     protected $course;
 
@@ -44,42 +48,43 @@ class lib_test extends \advanced_testcase {
         $this->resetAfterTest(true);
 
         $this->course = $this->getDataGenerator()->create_course();
-        $gpr = new grade_plugin_return();
-        $context = context_course::instance($this->course->id);
+        $gpr = new \grade_plugin_return();
+        $context = \context_course::instance($this->course->id);
         $letters = [1, 2, 3, 4, 5];
         $page = 0;
         $sortitemid = 0;
 
-        $this->report = new grade_report_gradedist($this->course->id, $gpr, $context, $letters, $page, $sortitemid);
+        $this->report = new \grade_report_gradedist($this->course->id, $gpr, $context, $letters, $page, $sortitemid);
     }
 
     /**
      * Test grade_report_gradedist constructor
-     * 
+     *
      * 2 Assertions
      */
     public function test_constructor() {
         $noreport = null;
 
-        // Assert: Check if the instance is created
+        // Assert: Check if the instance is created.
         $this->assertInstanceOf('grade_report_gradedist', $this->report);
-        
-        // Assert: Check if the instance is not created
+        $this->assertEquals($this->course->id, $this->report->courseid);
+
+        // Assert: Check if the instance is not created.
         $this->assertNull($noreport);
     }
 
     /**
      * Test grade_report_gradedist::load_users
-     * 
+     *
      * 4 Assertions
      */
     public function test_load_users() {
-        // Test no users
+        // Test no users.
         $this->assertEquals([], $this->report->load_users());
         $this->assertEmpty($this->report->load_users());
 
-        // Add users to the course
-        // Used manual users and not the data generator to make sure the sort order is correct
+        // Add users to the course.
+        // Used manual users and not the data generator to make sure the sort order is correct.
         $user1 = ['id' => 1, 'username' => 'user1', 'firstname' => 'fn1', 'lastname' => 'ln1', 'email' => 'user1@example.com'];
         $this->getDataGenerator()->enrol_user($user1['id'], $this->course->id);
         $user2 = ['id' => 2, 'username' => 'user2', 'firstname' => 'fn2', 'lastname' => 'ln2', 'email' => 'user2@example.com'];
@@ -87,106 +92,113 @@ class lib_test extends \advanced_testcase {
 
         $users = $this->report->load_users();
 
-        // Test 2 users in the course
+        // Test 2 users in the course.
         $this->assertEquals(2, count($users));
 
-        // Test 2 users in the course sorted
+        // Test 2 users in the course sorted.
         $this->assertEquals([$user1['id'], $user2['id']], array_keys($users));
     }
 
     /**
      * Test grade_report_gradedist::get_gradeitems
-     * 
+     *
      * 2 Assertions
      */
     public function test_get_gradeitems() {
-        // Test only 1 grade item (course sum)
+        // Test only 1 grade item (course sum).
         $result = $this->report->get_gradeitems();
         $this->assertEquals(1, count($result));
-        
+
         $gradeitem1 = $this->getDataGenerator()->create_grade_item([
-            'courseid' => $this->course->id, 
-            'itemtype' => 'manual', 
-            'itemname' => 'Test grade item 1', 
+            'courseid' => $this->course->id,
+            'itemtype' => 'manual',
+            'itemname' => 'Test grade item 1',
             'sortorder' => 1,
             'gradetype' => 1,
         ]);
         $gradeitem2 = $this->getDataGenerator()->create_grade_item([
-            'courseid' => $this->course->id, 
-            'itemtype' => 'manual', 
-            'itemname' => 'Test grade item 2', 
+            'courseid' => $this->course->id,
+            'itemtype' => 'manual',
+            'itemname' => 'Test grade item 2',
             'sortorder' => 1,
             'gradetype' => 1,
         ]);
 
-        // Reinstantiate the report to update gtree
-        $this->report = new grade_report_gradedist($this->course->id, new grade_plugin_return(), context_course::instance($this->course->id), [1, 2, 3, 4, 5], 0, 0);
+        // Reinstantiate the report to update gtree.
+        $this->report = new \grade_report_gradedist(
+            $this->course->id,
+            new \grade_plugin_return(),
+            \context_course::instance($this->course->id),
+            [1, 2, 3, 4, 5],
+            0,
+            0
+        );
         $result = $this->report->get_gradeitems();
 
-        // Test 3 grade items (course sum and 2 manual items)
+        // Test 3 grade items (course sum and 2 manual items).
         $this->assertEquals(3, count($result));
     }
 
     /**
      * Test grade_report_gradedist::get_grouplist
-     * 
+     *
      * 4 Assertions
      */
     public function test_get_grouplist() {
-        // Create a course
+        // Create a course.
         $course = $this->getDataGenerator()->create_course();
 
-        // Set courseid in the object under test
+        // Set courseid in the object under test.
         $this->report->courseid = $course->id;
 
-        // Create groups for the course
+        // Create groups for the course.
         $group1 = $this->getDataGenerator()->create_group(['courseid' => $course->id]);
         $group2 = $this->getDataGenerator()->create_group(['courseid' => $course->id]);
 
         $result = $this->report->get_grouplist();
 
-        // Assert that $result is as expected
-        $this->assertCount(3, $result); // Assert that there are 3 groups (2 created + 1 for all participants)
+        // Assert that $result is as expected.
+        $this->assertCount(3, $result); // Assert that there are 3 groups (2 created + 1 for all participants).
 
-        // Assert that the groups in $result match the groups we created
+        // Assert that the groups in $result match the groups we created.
         $this->assertEquals($group1->id, $result[$group1->id]->id);
         $this->assertEquals($group2->id, $result[$group2->id]->id);
-        $this->assertEquals(0, $result[0]->id); // This is the "all participants" group
+        $this->assertEquals(0, $result[0]->id); // This is the "all participants" group.
     }
 
     /**
      * Test grade_report_gradedist::get_groupinglist
-     * 
+     *
      * 3 Assertions
      */
     public function test_get_groupinglist() {
-        // Create a course
+        // Create a course.
         $course = $this->getDataGenerator()->create_course();
 
-        // Set courseid in the object under test
+        // Set courseid in the object under test.
         $this->report->courseid = $course->id;
 
-        // Create groupings for the course
+        // Create groupings for the course.
         $grouping1 = $this->getDataGenerator()->create_grouping(['courseid' => $course->id]);
         $grouping2 = $this->getDataGenerator()->create_grouping(['courseid' => $course->id]);
 
         $result = $this->report->get_groupinglist();
 
-        // Assert that $result is as expected
-        $this->assertCount(3, $result); // Assert that there are 3 groupings (2 created + 1 for no grouping)
+        // Assert that $result is as expected.
+        $this->assertCount(3, $result); // Assert that there are 3 groupings (2 created + 1 for no grouping).
 
-        // Assert that the groupings in $result match the groupings we created
+        // Assert that the groupings in $result match the groupings we created.
         $this->assertEquals($grouping1->id, $result[$grouping1->id]->id);
         $this->assertEquals($grouping2->id, $result[$grouping2->id]->id);
     }
 
     /**
      * Test grade_report_gradedist::load_distribution
-     * 
+     *
      * 6 Assertions
      */
     public function test_load_distribution() {
-        // Test no distribution
+        // Test no distribution.
         $letters = [
             90 => 1,
             80 => 2,
@@ -195,11 +207,11 @@ class lib_test extends \advanced_testcase {
             0 => 5,
         ];
 
-        // Test no entries
+        // Test no entries.
         $result = $this->report->load_distribution($letters);
         $this->assertEquals([0, 0, 0], $result->coverage);
 
-        // Add users to the course
+        // Add users to the course.
         $user1 = $this->getDataGenerator()->create_user();
         $user2 = $this->getDataGenerator()->create_user();
         $user3 = $this->getDataGenerator()->create_user();
@@ -207,44 +219,51 @@ class lib_test extends \advanced_testcase {
         $this->getDataGenerator()->enrol_user($user2->id, $this->course->id);
         $this->getDataGenerator()->enrol_user($user3->id, $this->course->id);
 
-        // Item to grade
+        // Item to grade.
         $gradeitem1 = $this->getDataGenerator()->create_grade_item([
-            'courseid' => $this->course->id, 
-            'itemtype' => 'manual', 
-            'itemname' => 'Test grade item 1', 
+            'courseid' => $this->course->id,
+            'itemtype' => 'manual',
+            'itemname' => 'Test grade item 1',
             'sortorder' => 1,
         ]);
 
-        // Grade the users
-        $grade_grade = new grade_grade([
+        // Grade the users.
+        $grade = new \grade_grade([
             'itemid' => $gradeitem1->id,
             'userid' => $user1->id,
             'finalgrade' => 24,
         ], false);
-        $grade_grade->insert();
-        $grade_grade = new grade_grade([
+        $grade->insert();
+        $grade = new \grade_grade([
             'itemid' => $gradeitem1->id,
             'userid' => $user2->id,
             'finalgrade' => 60,
         ], false);
-        $grade_grade->insert();
-        $grade_grade = new grade_grade([
+        $grade->insert();
+        $grade = new \grade_grade([
             'itemid' => $gradeitem1->id,
             'userid' => $user3->id,
             'finalgrade' => 85,
         ], false);
-        $grade_grade->insert();
+        $grade->insert();
 
-        // Reinstantiate the report to update gtree
-        $this->report = new grade_report_gradedist($this->course->id, new grade_plugin_return(), context_course::instance($this->course->id), [1, 2, 3, 4, 5], 0, 0);
-        
+        // Reinstantiate the report to update gtree.
+        $this->report = new \grade_report_gradedist(
+            $this->course->id,
+            new \grade_plugin_return(),
+            \context_course::instance($this->course->id),
+            [1, 2, 3, 4, 5],
+            0,
+            0
+        );
+
         $result = $this->report->load_distribution($letters, $gradeitem1->id);
-        // Used for debugging: print_r($result);
+        // Used for debugging @code: print_r($result);.
 
-        // Check coverage (items not included by new letters) 
+        // Check coverage (items not included by new letters).
         $this->assertEquals([0, 3, 0], $result->coverage);
 
-        // Check distribution (number of items per grade)
+        // Check distribution (number of items per grade).
         $this->assertEquals(0, $result->distribution[1]->count);
         $this->assertEquals(1, $result->distribution[2]->count);
         $this->assertEquals(0, $result->distribution[3]->count);
@@ -254,7 +273,7 @@ class lib_test extends \advanced_testcase {
 
     /**
      * Test grade_report_gradedist::get_gradeletter
-     * 
+     *
      * 4 Assertions
      */
     public function test_get_gradeletter() {
@@ -267,26 +286,26 @@ class lib_test extends \advanced_testcase {
         ];
 
         $gradeitem1 = $this->getDataGenerator()->create_grade_item([
-            'courseid' => $this->course->id, 
+            'courseid' => $this->course->id,
         ]);
 
-        $grade1 = new stdClass ();
+        $grade1 = new \stdClass ();
         $grade1->finalgrade = null;
         $grade1->itemid = $gradeitem1->id;
         $grade1->userid = 1;
 
-        // Test finalgrade is null
+        // Test finalgrade is null.
         $this->assertEquals('-', $this->report->get_gradeletter($letters, $grade1));
 
-        // Test finalgrade is 5
+        // Test finalgrade is 5.
         $grade1->finalgrade = 24;
         $this->assertEquals('5', $this->report->get_gradeletter($letters, $grade1));
 
-        // Test finalgrade is 1
+        // Test finalgrade is 1.
         $grade1->finalgrade = 100;
         $this->assertEquals('1', $this->report->get_gradeletter($letters, $grade1));
 
-        // Test wrong/no item id
+        // Test wrong/no item id.
         $grade1->itemid = null;
         $this->assertEquals('-', $this->report->get_gradeletter($letters, $grade1));
     }
