153aa155e33cba0c518a52c33dbf4563d66f98cb
marxc01 <marx.clemens@univie.ac.at>
2024-02-22 18:07:35 +0100

    Fix pipeline error

--- tests/lib_test.php
+++ tests/lib_test.php
@@ -16,6 +16,8 @@
 
 namespace gradereport_gradedist;
 
+use grade_plugin_return;
+
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
@@ -48,7 +50,7 @@ class lib_test extends \advanced_testcase {
         $this->resetAfterTest(true);
 
         $this->course = $this->getDataGenerator()->create_course();
-        $gpr = new \grade_plugin_return();
+        $gpr = new grade_plugin_return();
         $context = \context_course::instance($this->course->id);
         $letters = [1, 2, 3, 4, 5];
         $page = 0;
